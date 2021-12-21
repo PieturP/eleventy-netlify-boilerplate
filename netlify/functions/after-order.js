b45b47815c18b22dd771dd95cc0a11a851ec60ed
Peter Schilleman <peter@eenengelswoord.nl>
2021-12-21 15:43:58 +0100

    Tryout after order hook

--- netlify/functions/after-order.js
+++ netlify/functions/after-order.js
@@ -56,7 +56,7 @@ async function insertOrder(rawData) {
   }
   const resp = await axios.post(
     `${process.env.DIRECTUS_API_HOST}/items/orders_new`, {
-      raw_data: rawData
+      raw_data: JSON.stringify(rawData)
     }
   , options);
   console.log('Inserting Order data');
