8ab82a7a3d48a351115878ae7c25043bef8c7d7a
Peter Schilleman <peter@eenengelswoord.nl>
2021-12-23 17:32:41 +0100

    Styling (WIP)

--- _data/pageItems.js
+++ _data/pageItems.js
@@ -11,6 +11,9 @@ module.exports = async () => {
     +`?fields=name,items.item:collection.*,items.item:snippets.name,items.item:snippets.content,items.collection,items.item:images`
   , options);
 
+
+  console.dir(resp.data[0].items);
+
   return resp.data
 
 };
