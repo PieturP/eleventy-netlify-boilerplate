7491eb9228ea172ac26224af742897ccbb847cf7
Peter Schilleman <peter@eenengelswoord.nl>
2022-01-19 10:00:23 +0100

    CMS move

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,17 +1,19 @@
-/*
-if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", user => {
-    if (!user) {
-      window.netlifyIdentity.on("login", () => {
-        document.location.href = "/admin/";
-      });
-    }
-  });
-}
-*/
+// Snipcart items counter badges / labels
 document.addEventListener('snipcart.ready', () => {
     Snipcart.store.subscribe(() => {
         var count = Snipcart.store.getState().cart.items.count;
         [...document.querySelectorAll('.snipcart-items-counter')].forEach(el => el.textContent = count ? count : null);
     });
+});
+
+
+// Spinners upon form submit
+document.addEventListener('DOMContentLoaded', () => {
+  [...document.querySelectorAll('form')].forEach(formEl => {
+    formEl.addEventListener('submit', e => {
+      console.log('form submitted');
+      document.documentElement.setAttribute('data-loading', true);
+// e.preventDefault();
+    })
+  });
 });
